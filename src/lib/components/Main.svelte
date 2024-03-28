2b14008211737108ccb384703345db67fc4ffa48
Jeremy <jihlucky7@gmail.com>
2024-03-28 15:28:38 -0500

    Drag and drop implementation and fresh readme

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -3,16 +3,19 @@
         <EmptyBoard />
     {:else}
         {#each data.boards[currentBoard].columns as {name, tasks}, i}
-            <Column name={name} tasks={tasks}/>
+            <Column name={name} tasks={tasks} on:updateTasks={handleUpdateTasks}/>
         {/each}
     {/if}
 </main>
 
 <script>
-    
     import EmptyBoard from "./main/EmptyBoard.svelte";
     import Column from "./main/Column.svelte";
-    
+
     export let data;
     export let currentBoard;
+
+    function handleUpdateTasks(event) {
+        console.log(event.detail);
+    }
 </script>
--- src/lib/components/main/Column.svelte
+++ src/lib/components/main/Column.svelte
@@ -1,8 +1,8 @@
 <section class="task-column">
     <h3><div></div><span>{name}</span></h3>
-    <div use:dndzone>
-        {#each tasks as {title, subtasks}}
-            <TaskWidget task={title} totalSubtasks={subtasks.length} />
+    <div use:dndzone={{ items: localTasks, type: 'TASK' }} on:consider={handleDrop} on:finalize={handleDrop}>
+        {#each localTasks as task (task.id)}
+            <TaskWidget task={task.title} totalSubtasks={task.subtasks.length} />
         {/each}
     </div>
 </section>
@@ -10,7 +10,21 @@
 <script>
     import TaskWidget from "./TaskWidget.svelte";
     import { dndzone } from 'svelte-dnd-action';
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
 
     export let name;
     export let tasks;
+    export let columnID;
+
+    let localTasks = tasks.map((task, index) => ({ id: index, ...task }));
+
+    function handleDrop(event) {
+        localTasks = event.detail.items;
+        
+        let updatedTasks = localTasks.map(({ id, ...task }) => task)
+
+        dispatch('updateTasks', updatedTasks);
+    }
 </script>
